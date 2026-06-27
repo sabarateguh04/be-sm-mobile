78786fe973f1c1ac9bedaa66d70636630f6a4133
sabarateguh04 <teguhsabara16@gmail.com>
2026-06-27 14:48:51 +0700

    upd cad

--- cad.route.js
+++ cad.route.js
@@ -10,7 +10,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const pool    = require('../db');
+const pool    = require('./db');
 const multer  = require('multer');
 const path    = require('path');
 const fs      = require('fs');
